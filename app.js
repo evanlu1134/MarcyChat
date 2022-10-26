b8abf23c8357e914e89bc75850f6331fc489b5f7
evanlu1134 <evanlu1134@gmail.com>
2022-10-26 10:51:37 -0400

    added all the setup and packages

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require("cors")
+const app = express();
+const PORT = process.env.PORT || 5432;
+
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(PORT, () => {
+    console.log(`listening http://localhost:${PORT}`)
+})
