86cd3de330a16428027bba9ade174a903ae51659
evanlu1134 <evanlu1134@gmail.com>
2022-10-28 09:08:39 -0400

    fixed the pathways and issues with modals

--- login.js
+++ login.js
@@ -2,7 +2,35 @@ const url = 'http://localhost:3000/login'
 const form = document.forms["create-todo-form"]
 const welcome= document.getElementById("welcome")
 const register=document.getElementById("register")
+const login = document.getElementById("load")
+// Get the modal
+const modal = document.getElementById('id01');
+const span = document.getElementsByClassName("close")[0];
+const btn = document.getElementById("btn")
+const cancelbtn = document.getElementById("cancelbtn")
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+btn.onclick = function(event) {
+  modal.style.display='block'
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+cancelbtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+
+login.addEventListener("click", () => {
 form.addEventListener("submit", (event) => {
     event.preventDefault()
     const email = document.querySelector("#email").value
@@ -24,28 +52,26 @@ form.addEventListener("submit", (event) => {
   };
 
   fetch(url, requestOptions)
-})
 
 
-form.addEventListener("submit", () => {
-const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+// window.location.href = "./index.html"
+const myHeaders2 = new Headers();
+myHeaders2.append("Content-Type", "application/json");
 
 
-const requestOptions = {
+const requestOptions2 = {
   method: 'GET',
-  headers: myHeaders,
+  headers: myHeaders2,
   redirect: 'follow'
 };
 
-fetch("http://localhost:3000/login", requestOptions)
-  .then(response => response.text())
+fetch("http://localhost:3000/login", requestOptions2)
+  .then(response => response.json())
   .then(result => {
     if(result!==null || result.length>0){
       console.log(result)
     form.style.display="none"
     register.style.display="none"
-   window.location.href = "./index.html"
   }else{
       form.style.display="none"
       register.style.display="none"}
@@ -53,4 +79,7 @@ fetch("http://localhost:3000/login", requestOptions)
     })
   
   .catch(error => console.log('error', error));
+
+})
+window.location.href = "./index.html"
 })
