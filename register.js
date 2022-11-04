df4d6b8d96be56245dfd5e00642426679595bc93
clopez11224 <christopherlopez000@gmail.com>
2022-11-04 18:36:31 -0400

    Changed spelling

--- register.js
+++ register.js
@@ -1,4 +1,4 @@
-const url = "https://scary-coffin-46937.herokuapp.com/auth/users";
+const url = "https://scary-coffin-46937.herokuapp.com/users";
 const form = document.forms["create-todo-form"];
 
 //Modal
